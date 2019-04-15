65ab4a7258b92224ffeec5b4be07e12d12b20955
Walter Coelho <wcoelho@gmail.com>
2019-04-15 00:18:44 -0300

    Corrigido path para api.service

--- src/service/api/api.service.spec.ts
+++ src/service/api/api.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ApiService } from '../service/api/api.service';
+import { ApiService } from './api.service';
 
 describe('ApiService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
